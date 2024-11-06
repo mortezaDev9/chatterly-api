eacbcf107c50db78cdab24d3da2b8dbc89629532
Morteza <morteza9.dev@gmail.com>
2024-11-06 14:41:36 +0330

    refactor: update user_id column to be nullable

--- Modules/User/database/migrations/0001_01_01_000000_create_users_table.php
+++ Modules/User/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id')->unique();
+            $table->string('user_id')->unique()->nullable();
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('phone')->unique();
