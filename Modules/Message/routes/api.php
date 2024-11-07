f66dbd4917d02765fa1e76698375bc60e9a989eb
Morteza <morteza9.dev@gmail.com>
2024-11-07 15:19:29 +0330

    refactor: remove unused coede

--- Modules/Message/routes/api.php
+++ Modules/Message/routes/api.php
@@ -1,19 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Message\Http\Controllers\MessageController;
-
-/*
- *--------------------------------------------------------------------------
- * API Routes
- *--------------------------------------------------------------------------
- *
- * Here is where you can register API routes for your application. These
- * routes are loaded by the RouteServiceProvider within a group which
- * is assigned the "api" middleware group. Enjoy building your API!
- *
-*/
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('message', MessageController::class)->names('message');
-});
